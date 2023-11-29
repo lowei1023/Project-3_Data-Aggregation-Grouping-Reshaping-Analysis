47b6b9202001d53c235116772cddfb25739a59db
lowei1023 <144854243+lowei1023@users.noreply.github.com>
2023-11-29 17:17:15 -0500

    'energy'

--- src/Energy_Analysis/__main__.py
+++ src/Energy_Analysis/__main__.py
@@ -1,5 +1,16 @@
+from .part_1_summary import Sum
+from .part_4_Inference import EnergyAnalysis
 def main():
-    print("hello Wei Lo")
+    print("hello Professor Stefanie")
+    path = [
+    "https://raw.githubusercontent.com/lowei1023/Sustainable_Energy_Transition_Analysis/main/electricity_con.csv",
+    "https://raw.githubusercontent.com/lowei1023/Sustainable_Energy_Transition_Analysis/main/electricity_gen.csv"
+    ]
+    merger = Sum(path)
+    df = merger.data_merger()
+    Inference = EnergyAnalysis(df)
+    Inference.task_1()
+
 
 
 main()
