614c2b040d6438e348ca2214268cc6a748f5b746
lowei1023 <144854243+lowei1023@users.noreply.github.com>
2023-11-29 22:58:37 -0500

    'energy'

--- src/Energy_Analysis/__main__.py
+++ src/Energy_Analysis/__main__.py
@@ -1,15 +1,15 @@
-from .part_1_summary import Sum
 from .part_4_Inference import EnergyAnalysis
+import pandas as pd
+
 def main():
     print("hello Professor Stefanie")
-    path = [
-    "https://raw.githubusercontent.com/lowei1023/Sustainable_Energy_Transition_Analysis/main/electricity_con.csv",
-    "https://raw.githubusercontent.com/lowei1023/Sustainable_Energy_Transition_Analysis/main/electricity_gen.csv"
-    ]
-    merger = Sum(path)
-    df = merger.data_merger()
+    path = ["https://raw.githubusercontent.com/lowei1023/Sustainable_Energy_Transition_Analysis/main/merged_electricity_data.csv"]
+    df = pd.read_csv(path)
     Inference = EnergyAnalysis(df)
     Inference.task_1()
+    Inference.task_2()
+    Inference.task_5()
+    Inference.task_6()
 
 
 
